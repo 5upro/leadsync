9d7a7c29a270794db22f32137fccf971b8bfd982
SUPRØ <00youareworngtho00@gmail.com>
2026-05-17 04:50:59 +0530

    fix(server): update base URL API endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use("/auth", authRouter);
+app.use("/api/auth", authRouter);
 
 app.use(errorHandler);
 
