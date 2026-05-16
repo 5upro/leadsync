4fc6cd419c0bab6a43d6d1d0cfea3a8ab1b806d7
SUPRØ <00youareworngtho00@gmail.com>
2026-05-17 00:28:41 +0530

    feat(server/middlewares): add global validator

---
+++ server/src/middlewares/global.validator.ts
@@ -0,0 +1,24 @@
+import type { 
+	Response, 
+	Request, 
+	NextFunction, 
+} from 'express';
+import { ZodType } from 'zod';
+
+const validator = <T>(schema: ZodType<T>) => 
+	(req: Request<{}, {}, T>, res: Response, next: NextFunction) => {
+		const parsed = schema.safeParse(req.body);
+
+		if (!parsed.success) {
+			return res.status(400).json({
+				error: parsed.error.issues.map((err) => ({
+					path: err.path.join("."),
+					message: err.message,
+				})),
+			});    
+		}
+		req.body = parsed.data;
+		next();
+	}
+
+export default validator;
