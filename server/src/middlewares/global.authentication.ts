282cb7bc4bbab8da29ae706a63007a5745299029
SUPRØ <00youareworngtho00@gmail.com>
2026-05-17 03:19:09 +0530

    feat(server/middlewares): add global authentication middleware

---
+++ server/src/middlewares/global.authentication.ts
@@ -0,0 +1,24 @@
+import type { 
+	Response, 
+	Request, 
+	NextFunction, 
+} from 'express';
+import { verifyAccessToken } from "@/utils/token";
+
+const authentication = (req: Request, res: Response, next: NextFunction) => {
+	const authHeader = req.headers.authorization;
+	if(!authHeader) {
+		return res.status(401).json({message: "No Token"});
+	}
+
+	const token = authHeader?.split(" ")[1];
+	try {
+		const decode = verifyAccessToken(token!);
+		req.user = decode;
+        next();
+	} catch (error) {
+        return res.status(401).json({message: "Invalid Token"});
+	}
+}
+
+export default authentication;
