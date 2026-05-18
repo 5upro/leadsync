f4b99f189ea0d99716630379434ff1175925c2c4
SUPRØ <00youareworngtho00@gmail.com>
2026-05-18 20:44:26 +0530

    feat(client): add tailwindcss config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import talwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), talwindcss()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
