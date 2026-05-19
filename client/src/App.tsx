f6e7e4a37fdb4f039721305549febb6ad5d5a26f
SUPRØ <00youareworngtho00@gmail.com>
2026-05-19 06:30:06 +0530

    feat(client): add router to root/entry point

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
-import { useState, type FC } from "react";
+import { 
+	useState, 
+	useEffect, 
+	type FC } from "react";
+import AppRouter from "@/router/AppRouter";
 import SplashScreen from "@/pages/SplashScreen";
 
+/**
+ * App Root
+ */
 const App: FC = () => {
-	return (
-		<SplashScreen/>
-	);
-}
+	const [splash, setSplash] = useState(true);
+
+	useEffect(() => {
+		const timer = setTimeout(() => setSplash(false), 2200);
+		return () => clearTimeout(timer);
+	}, []);
+
+	if (splash) return <SplashScreen />;
+
+	return <AppRouter />;
+};
 
 export default App;
