5eafb7c46cd924ffe2599606f6f40bd10992aaf1
SUPRØ <00youareworngtho00@gmail.com>
2026-05-19 06:32:48 +0530

    feat(client): set default port 3000

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,9 @@ import talwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	plugins: [react(), talwindcss()],
+	server: {
+        port: 3000,
+	},
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
